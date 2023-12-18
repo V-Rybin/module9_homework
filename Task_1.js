018cc6b2b1eedfaa255ac1f1b6e0930564d5c910
V-Rybin <146269407+V-Rybin@users.noreply.github.com>
2023-12-18 22:26:40 +0300

    Update Task_1.js

--- Task_1.js
+++ Task_1.js
@@ -1,18 +1,39 @@
-const jsonString =  `{
-    "list": [
-     {
-      "name": "Petr",
-      "age": "20",
-      "prof": "mechanic"
-     },
-     {
-      "name": "Vova",
-      "age": "60",
-      "prof": "pilot"
-     }
-    ]
-   }`;
+const parser = new DOMParser();
 
-const result = JSON.parse(jsonString);
+const xmlString = `
+  <person category="grown man">
+    <name lang="en">Ivan</name>
+    <second>Ivanov</second>
+    <age>35</age>
+    <prof>teacher</prof>
+   </person>
+  <person category="grown man">
+    <name lang="ru">Петр</name>
+    <second>Петров</second>
+    <age>58</age>
+    <prof>driver</prof>
+  </person>
 
-console.log(result);
+  `;
+
+
+const xmlDOM = parser.parseFromString(xmlString, "text/xml");
+
+
+const personNode = xmlDOM.querySelector("person");
+const nameNode = personNode.querySelector("name");
+const secondNode = personNode.querySelector("second");
+const ageNode = personNode.querySelector("age");
+const profNode = personNode.querySelector("prof");
+
+const langAttr = nameNode.getAttribute('lang');
+
+const list = {
+
+  name: nameNode.textContent,
+  age: Number(ageNode.textContent),
+  prof: profNode.textContent,
+  lang: langAttr,
+  
+};
+console.log('list', list);
